040de94c7933f6cf50620d28219f5826d4fe8081
Dhaval Vaitha <vaithadhaval@gmail.com>
2021-06-17 08:38:30 +0530

    minor changes commit of 17th june 2021

--- ShoppingWebParent/ShoppingBackEnd/src/test/java/com/ecommerce/admin/user/RoleRepositoryTests.java
+++ ShoppingWebParent/ShoppingBackEnd/src/test/java/com/ecommerce/admin/user/RoleRepositoryTests.java
@@ -24,13 +24,7 @@ public class RoleRepositoryTests {
 	@Test
 	public void testCreateFirstRole() {
 		Role roleAdmin = new Role("Admin", "manage Everything");
-		Role savedRole = repository.save(roleAdmin);
 
-		assertThat(savedRole.getId()).isGreaterThan(0);
-	}
-
-	@Test
-	public void testCreateRestRoles() {
 		Role roleSalesperson = new Role("Salesperson",
 				"manage product price, customers, shipping, orders and sales report");
 
@@ -40,6 +34,12 @@ public class RoleRepositoryTests {
 
 		Role roleAssistant = new Role("Assistant", "manage questions and reviews");
 
-		repository.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
+		repository.saveAll(List.of(roleAdmin, roleSalesperson, roleEditor, roleShipper, roleAssistant));
+
+	}
+
+	@Test
+	public void testCreateRestRoles() {
+
 	}
 }
